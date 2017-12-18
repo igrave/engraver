f68bff863708492bbb6ade803176c9333f1862af
Isaac Gravestock <Isaac.Gravestock@usz.ch>
2017-12-18 15:11:57 +0100

    Remove old code and add documentation

--- WordProps/R/loadProperties.R
+++ WordProps/R/loadProperties.R
@@ -1,28 +1,11 @@
 
-#' loadProperties
-#'
-#' @param file A .docx file to read the data from
-#'
-#' @return A list of nodes
-#'
-# loadProperties <- function(file){
-# 
-# 
-# 
-# 
-#   if (!is.null(path) && !file.exists(path))
-#     stop("could not find file ", shQuote(path), call. = FALSE)
-#   if (is.null(path))
-#     path <- system.file(package = "officer", "template/template.docx")
-#   package_dir <- tempfile()
-#   unpack_folder(file = path, folder = package_dir)
-# 
-# 
-# 
-# }
 
 
-
+#' Read custom.xml from rdocx object 
+#'
+#' @param rdocx object
+#'
+#' @return xml object
 read_from_rdocx <- function(rdocx){
-  xml2::read_xml(x=paste0(rdocx$package_dir,"\\docProps\\custom.xml"))
+  read_xml(x=file.path(rdocx$package_dir,"docProps","custom.xml"))
 }
