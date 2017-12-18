d4f1ee59eb079bc9a7bf2fbe9961d42f2c05ded5
Isaac Gravestock <Isaac.Gravestock@usz.ch>
2017-12-18 15:11:33 +0100

    Add documentation

--- WordProps/R/fieldFunctions.R
+++ WordProps/R/fieldFunctions.R
@@ -1,3 +1,12 @@
+#' Insert property into Word document
+#'
+#' @param name Property name
+#' @param xml xml object with corresponding property definition
+#' @param doc rdocx object
+#' @param options field format options. (Default "\\* MERGEFORMAT" to keep formatting)
+#' @param ... Other parameters passed to officer::slip_in_seqfield()
+#'
+#' @return Nothing
 slip_in_docprop_field <- function(name, xml, doc, options ="\\* MERGEFORMAT", ...){
   #look at slip in seqfield
   if(!missing(xml) & !(name %in% get_names(xml))) warning("Docproperty name not in xml")
