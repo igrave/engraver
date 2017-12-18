82157e2d8d07b96fa0c86da2d8bca328b146730b
Isaac Gravestock <Isaac.Gravestock@usz.ch>
2017-12-18 15:12:19 +0100

    Remove old code and add documentation

--- WordProps/R/saveProperties.R
+++ WordProps/R/saveProperties.R
@@ -1,20 +1,12 @@
 
-#' saveProperties
-#'
-#' @param file A .docx file to save the data to
-#' @param properties The list to save
-#' @param sourcefile A file to copy from and overwrite with the properties (Optional)
-#'
-#' @return A .docx file with the custom properties.
-#'
-# saveProperties <- function(file, properties, sourcefile = file){
-# 
-#   tempfile()
-# 
-#   list(properites = 1)
-# }
 
 
+#' Save xml object to rdocx object
+#'
+#' @param rdocx object
+#' @param xml object
+#'
+#' @return nothing
 save_to_rdocx <- function(rdocx, xml){
-  xml2::write_xml(xml, file=paste0(rdocx$package_dir,"\\docProps\\custom.xml"))
+  write_xml(xml, file.path(rdocx$package_dir,"docProps","custom.xml"))
 }
