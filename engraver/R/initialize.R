409ca13f031aa5f18c8e9f70038dc1681eed1e5a
Isaac Gravestock <Isaac.Gravestock@usz.ch>
2018-07-04 07:34:41 +0200

    initialise function is ready

--- engraver/R/initialize.R
+++ engraver/R/initialize.R
@@ -1,18 +1,43 @@
 
 
+#' Initialise a document for use with engraver
+#'
+#' @param rdocx 
+#'
+#' @return Nothing returned. The .rels and Content_Types files are updated.
 initialize_docx <- function(rdocx){
   
+  #add to the rels file
   rels <- read_xml(x=file.path(rdocx$package_dir,"_rels",".rels"))
+  xml_add_child(rels,
+                .value="Relationship",
+                Id="rId4",
+                Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/custom-properties",
+                Target="docProps/custom.xml")
   
-  content <- read_xml(x=file.path(rdocx$package_dir,"[Content_Types].xml"))
+  
+  write_xml(rels,
+            file = file.path(rdocx$package_dir,"_rels",".rels"),
+            options="")
+
+  # #add to the contents file  
+  # content <- read_xml(x=file.path(rdocx$package_dir,"[Content_Types].xml"))
+  # 
+  # xml_add_child(content,
+  #               .value="Override",
+  #               PartName="/docProps/custom.xml",
+  #               ContentType="application/vnd.openxmlformats-officedocument.custom-properties+xml")
+  # 
+  # write_xml(content,
+  #           file = file.path(rdocx$package_dir,"[Content_Types].xml"),
+  #           options="")
+  
+  mydoc$content_type$add_override(
+    setNames("application/vnd.openxmlformats-officedocument.custom-properties+xml",
+             "/docProps/custom.xml"))
+  
+  
+  
 }
 
 
-debug(initialize_docx)
-
-initialize_docx(mydoc)
-
-xml2::xml_add_child(rels,
-                    .value="Relationship",
-                    Id="rId4",
-                    Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/custom-properties" Target="docProps/custom.xml")
