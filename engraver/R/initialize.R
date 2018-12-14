0f4ba27f29bf9a806b7a516abedf51dc85c04a0a
Gravestock Isaac <Isaac.Gravestock@usz.ch>
2018-12-14 12:45:45 +0100

    upgraded initialise function

--- engraver/R/initialize.R
+++ engraver/R/initialize.R
@@ -1,25 +1,33 @@
-
-
 #' Initialise a document for use with engraver
 #'
-#' @param rdocx 
+#' @param rdocx An rdocx created by Officer's read_docx
 #'
 #' @return Nothing returned. The .rels and Content_Types files are updated.
 initialize_docx <- function(rdocx){
   
-  #add to the rels file
+  #Adding to the _rels/.rels XML file ---------------------------------------------
+  
   rels <- read_xml(x=file.path(rdocx$package_dir,"_rels",".rels"))
-  xml_add_child(rels,
-                .value="Relationship",
-                Id="rId4",
-                Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/custom-properties",
-                Target="docProps/custom.xml")
   
+  # Do any of the children have custom.xml?
+  if(!any(unlist(xml_attrs(xml_children(rels)))=="docProps/custom.xml")){
+    Ids <- sapply(xml_attrs(xml_children(rels)),`[`,"Id")
+    max_Id_num <- max(as.numeric(substr(sapply(xml_attrs(xml_children(rels)),`[`,"Id"), start = 4,stop=6)))
+    next_id <- paste0('rId',max_Id_num+1)
+    
+    xml_add_child(rels,
+                  .value="Relationship",
+                  Id=next_id,
+                  Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/custom-properties",
+                  Target="docProps/custom.xml")
+    
+    
+    write_xml(rels,
+              file = file.path(rdocx$package_dir,"_rels",".rels"),
+              options="")
+    
+  }
   
-  write_xml(rels,
-            file = file.path(rdocx$package_dir,"_rels",".rels"),
-            options="")
-
   # #add to the contents file  
   # content <- read_xml(x=file.path(rdocx$package_dir,"[Content_Types].xml"))
   # 
@@ -32,12 +40,14 @@ initialize_docx <- function(rdocx){
   #           file = file.path(rdocx$package_dir,"[Content_Types].xml"),
   #           options="")
   
-  mydoc$content_type$add_override(
-    setNames("application/vnd.openxmlformats-officedocument.custom-properties+xml",
+  # Is it already in the Content_Types xml?
+  if(!any(unlist(xml_attrs(xml_children(read_xml(x=file.path(rdocx$package_dir,"[Content_Types].xml")))))
+          == "/docProps/custom.xml")){
+    rdocx$content_type$add_override(value = setNames("application/vnd.openxmlformats-officedocument.custom-properties+xml",
              "/docProps/custom.xml"))
+    # rdocx$content_type$save()
+  }
+
   
-  
-  
+  rdocx
 }
-
-
